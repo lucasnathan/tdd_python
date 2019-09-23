1bf8676bee01164eb0f575a3253fe925d5dccc78
Lucas Nathan Barbosa de Oliveira <lucas.nathan80@hotmail.com>
2019-09-23 16:55:02 -0300

    Front page HTML now generated grom template
    Corrections on functional_tests - typo, forget attribute;
    Corrections on home.html - modifications to fit the functional test cases;

--- functional_tests.py
+++ functional_tests.py
@@ -18,29 +18,26 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         # On the page title mentions the to-do list
-        self.assertIn('To-Do', self.browser.title)
+        self.assertIn('To-Do lists', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
+        self.assertIn('Your To-Do list', header_text)
         
 
         # He receives the input to enter a to-do item right away
-        inputbox = self.browser.find_elements_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
         # He types "Buy peacock feathers" into a text box (Marquinhos's hobby is collecting peacock feathers)
         inputbox.send_keys('Buy peacock feathers')
 
         # When he hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
-        inputbox.send_keys(keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue (
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table"
         )
         # There is still a text box inviting him to add another item. He
         # enters "Use peacock feathers to make me fly" (Marquinhos is very excentric)
